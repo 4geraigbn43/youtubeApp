5ccbf66448f3b8265dc6a6a2809e63d456824916
unknown <anefary396gmail.com>
2023-11-20 09:49:24 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import numpy as np 
+import pandas as pd 
+from PIL import Image
+import time 
+st.title('Streamlit 超入門')
+
+st.write('プログレスバーの表示')
+'Start!!'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration{i + 1}')
+    bar.progress(i +1)
+    time.sleep(0.1)
+
+left_column, right_column = st.columns(2)
+button = left_column.button('右カラムに文字を表示')
+if button:
+    right_column.write('ここは右カラム')
+
+expander = st.expander('問い合わせ')
+expander.write('問い合わせ内容を書く')
+
+# option = st.text_input('あなたの趣味を教えてください.')
+# 'あなたの趣味：', option, 'です'
+
+# condition = st.slider('あなたの今の調子は?', 0, 100, 50)
+# 'コンディション:', condition 
+
+
+# img = Image.open('pikachu.png')
+ #   st.image(img, caption='Pikachu', use_column_width=True)
+
